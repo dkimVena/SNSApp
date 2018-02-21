a2e0e8a2371cdb4d6ef8e7e7631b4e106b55b854
goginim <david.ingook@gmail.com>
2018-02-21 13:43:08 -0500

    before deploy

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -3,11 +3,8 @@ var path = require('path');
 
 module.exports = {
 
-    /* webpack-dev-server를 콘솔이 아닌 자바스크립트로 실행 할 땐,
-    HotReloadingModule 를 사용하기 위해선 dev-server 클라이언트와
-    핫 모듈을 따로 entry 에 넣어주어야 합니다. */
-
     entry: [
+        'babel-polyfill',
         './src/index.js',
         'webpack-dev-server/client?http://0.0.0.0:4000',
         'webpack/hot/only-dev-server',
@@ -15,24 +12,21 @@ module.exports = {
     ],
 
     output: {
-        path: '/', // public 이 아니고 /, 이렇게 하면 파일을 메모리에 저장하고 사용합니다
+        path: '/',
         filename: 'bundle.js'
     },
 
-    // 개발서버 설정입니다
     devServer: {
         hot: true,
         filename: 'bundle.js',
         publicPath: '/',
         historyApiFallback: true,
         contentBase: './public',
-        /* 모든 요청을 프록시로 돌려서 express의 응답을 받아오며,
-        bundle 파일의 경우엔 우선권을 가져서 devserver 의 스크립트를 사용하게 됩니다 */
         proxy: {
-            "**": "http://localhost:3000" // express 서버주소
+            "*": "http://localhost:3000"
         },
         stats: {
-          // 콘솔 로그를 최소화 합니다
+          // Config for minimal console.log mess.
           assets: false,
           colors: true,
           version: false,
@@ -45,31 +39,29 @@ module.exports = {
 
 
     plugins: [
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin()
+        new webpack.HotModuleReplacementPlugin()
     ],
 
     module: {
-      loaders: [
-          {
-              test: /.js$/,
-              loader: 'babel',
-              exclude: /node_modules/,
-              query: {
-                  cacheDirectory: true,
-                  presets: ['es2015', 'react'],
-                  plugins: ["react-hot-loader/babel"]
-              }
-          },
-          {
-              test: /\.css$/,
-              loader: 'style!css-loader'
-          }
-      ]
+        loaders: [
+            {
+                test: /\.js$/,
+                loaders: ['react-hot', 'babel?' + JSON.stringify({
+                    cacheDirectory: true,
+                    presets: ['es2015', 'react']
+                })],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                loader: 'style!css-loader'
+            }
+        ]
     },
 
     resolve: {
         root: path.resolve('./src')
     }
+
+
 };
