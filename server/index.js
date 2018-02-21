9d1e2441cfa73a2c00f4aaafb4d6fee75b7c26e8
goginim <david.ingook@gmail.com>
2018-02-21 10:36:07 -0500

    fix

--- server/index.js
+++ server/index.js
@@ -44,19 +44,6 @@ app.use(function(err, req, res, next) {
   res.status(500).send('Something broke!');
 });
 
-if (process.env.NODE_ENV === 'production') {
-  // Express will serve up production asssets
-  // like our main.js file, or main.css file!!
-  app.use(express.static('client/build'));
-
-  // Express will serve up the index.html file
-  // if it doesnt' recognize the routes
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
 app.listen(port, () => {
     console.log('Express is listening on port', port);
 });
@@ -71,4 +58,15 @@ if(process.env.NODE_ENV == 'development') {
             console.log('webpack-dev-server is listening on port', devPort);
         }
     );
+} else if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production asssets
+  // like our main.js file, or main.css file!!
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file
+  // if it doesnt' recognize the routes
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
