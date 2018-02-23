f01374175a3f873aaef17419e2a6fb589e7f7ba4
goginim <david.ingook@gmail.com>
2018-02-23 01:10:41 -0500

    before deploy

--- webpack.config.js
+++ webpack.config.js
@@ -40,6 +40,7 @@ module.exports = {
           }
         }),
         new webpack.optimize.UglifyJsPlugin({
+          minimize: true,
           compress:{
             warnings: true
           }
